789265bc27218f65fc92ebbf804943fb8b1f272d
olegpl1993 <olegpl1993@gmail.com>
2023-07-06 01:31:25 +0300

    feat: install options and dependen dependencies

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  mode: 'production',
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externalsPresets: { node: true },
+  externals: [nodeExternals()],
+};
