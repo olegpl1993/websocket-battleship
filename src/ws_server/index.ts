7da72c2fa7c5e7bc68503e3725fd9d022c3fa81a
olegpl1993 <olegpl1993@gmail.com>
2023-07-08 18:31:04 +0300

    refactor: variables name

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -11,20 +11,20 @@ export const server = new WebSocket.Server({ port: 3000 });
 server.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.on('message', (message) => {
-    const messageString = message.toString(); // Преобразование буфера в строку
-    const messageObject: ClientServerMessage = JSON.parse(messageString); // Преобразование строки в объект
-    const action = actionList[messageObject.type]; // Выбор действия
-    const dataObj = action(messageObject); // Вызов действия создающего обьект дата
-    const data = JSON.stringify(dataObj); // Преобразование обьекта дата в строку
-    // сообщение с ответом для клиента
-    const response: ClientServerMessage = {
-      type: messageObject.type,
+  socket.on('message', (request) => {
+    const requestString = request.toString(); // Преобразование буфера в строку
+    const ClientMessage: ClientServerMessage = JSON.parse(requestString); // Преобразование строки в объект
+    const action = actionList[ClientMessage.type]; // Выбор действия
+    const dataObj = action(ClientMessage); // data для ответа
+    const data = JSON.stringify(dataObj); // data в строку
+    // ответ для клиента
+    const ServerMessage: ClientServerMessage = {
+      type: ClientMessage.type,
       data,
       id: 0,
     };
-    const stringResponse = JSON.stringify(response); // Преобразование ответа в строку
-    socket.send(stringResponse); // Отправка ответа
+    const response = JSON.stringify(ServerMessage); // ответ в строку
+    socket.send(response); // Отправка ответа
   });
 
   socket.on('close', () => {
--- src/ws_server/reg.ts
+++ src/ws_server/reg.ts
@@ -1,43 +1,43 @@
 import { users } from './users';
 import { ClientServerMessage, UserData } from './types';
 
-export const reg = (messageObject: ClientServerMessage) => {
-  const userData: UserData = JSON.parse(messageObject.data);
+export const reg = (ClientMessage: ClientServerMessage) => {
+  const data: UserData = JSON.parse(ClientMessage.data);
 
-  const userExists = users.find((user) => user.name === userData.name); // Проверка существования
-  const isCorrectPassword = userExists?.password === userData.password; // Проверка пароля
-  const index = users.findIndex((user) => user.name === userData.name); // Индекс пользователя
+  const userExists = users.find((user) => user.name === data.name); // Проверка существования
+  const isCorrectPassword = userExists?.password === data.password; // Проверка пароля
+  const index = users.findIndex((user) => user.name === data.name); // Индекс пользователя
 
   // Если пользователь существует и пароль совпадает
   if (userExists && isCorrectPassword) {
-    const objData = {
-      name: userData.name,
+    const responseData = {
+      name: data.name,
       index,
       error: false,
       errorText: '',
     };
-    return objData;
+    return responseData;
   }
 
   // Если пользователь существует и пароль не совпадает
   if (userExists && !isCorrectPassword) {
-    const objData = {
-      name: userData.name,
+    const responseData = {
+      name: data.name,
       index,
       error: true,
       errorText: 'Invalid password',
     };
-    return objData;
+    return responseData;
   }
 
   // Если пользователь не существует
-  users.push(userData); // Добавляем нового пользователя
+  users.push(data); // Добавляем нового пользователя
   const newIndex = users.length - 1; // Индекс добавленного пользователя
-  const objData = {
-    name: userData.name,
+  const responseData = {
+    name: data.name,
     index: newIndex,
     error: false,
     errorText: '',
   };
-  return objData;
+  return responseData;
 };
